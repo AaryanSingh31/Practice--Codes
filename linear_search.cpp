bfb2e50e47a508c010a78bbbc62a876a77f9efd1
AaryanSingh31 <www.aaryan000singh@gmail.com>
2025-05-21 10:07:54 +0530

    updated linear search

--- linear_search.cpp
+++ linear_search.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 using namespace std;
 int linearSearch(int arr[], int size, int target){  
@@ -21,29 +20,4 @@ int main(){
 }
 
 // output :-
-=======
-#include <iostream>
-using namespace std;
-int linearSearch(int arr[], int size, int target){  
-
-    for (int i = 0; i < size; i++){
-        if (arr[i] == target){
-            return i;                        // If found. 
-        }
-    }
-    return -1;                               // If not found.
-}
-int main(){
-    int arr[] = {2, 45, 10, 22, 8, 90};
-    int size = 6;
-    int target = 8;
-
-    cout << linearSearch(arr, size, target);  //calling the fn.
-
-    return 0;
-}
-//little change
-
-// output :-
->>>>>>> bde8109 (Add linear search)
 //  index => 4
